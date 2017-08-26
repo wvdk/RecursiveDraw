7e35bfb3f021e489163241fe9e31f2d869b50f1e
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-26 12:00:08 +0300

    get scene rendering

--- RecursiveDraw macOS/GameViewController.swift
+++ RecursiveDraw macOS/GameViewController.swift
@@ -11,19 +11,47 @@ import SpriteKit
 
 class GameViewController: NSViewController {
 
+    var shapesInventory: ShapeInventory = [:]
+    var rootShape: ShapeName?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene.newGameScene()
         
+        
+//        let scene = GameScene.newGameScene()
         // Present the scene
         let skView = self.view as! SKView
-        skView.presentScene(scene)
+//        skView.presentScene(scene)
+//
+//        skView.ignoresSiblingOrder = true
+//
+//        skView.showsFPS = true
+//        skView.showsNodeCount = true
+//
+//
+//
+        let size = CGSize(width: 100, height: 100)
+        let shapePosition = CGPoint(x: 75, y: 90)
         
-        skView.ignoresSiblingOrder = true
+        shapesInventory["square"] = Shape(size: size, position: shapePosition, children: ["square"])
         
-        skView.showsFPS = true
-        skView.showsNodeCount = true
+        let renderer = SpriteKitRenderer(shapesInventory: shapesInventory)
+        let rendered = renderer.render(shapeNamed: "square")
+        
+        
+            let scene = GameScene(size: skView.frame.size)
+            
+            scene.scaleMode = .aspectFit
+            
+            scene.addChild(rendered)
+            
+            skView.presentScene(scene)
+            
+            skView.ignoresSiblingOrder = true
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+        
     }
 
 }
